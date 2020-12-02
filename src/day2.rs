b28250cc114bac1453d2f01384594c1d6ab73e11
Ben Cohen <ben@cohen-family.org>
2020-12-02 15:39:36 -0800

    day 1 and 2

---
+++ src/day2.rs
@@ -0,0 +1,45 @@
+use std::io::{self, BufRead};
+use regex::Regex;
+use std::fs;
+use aoc::soln;
+
+#[soln]
+pub fn day2() {
+    if let Ok(file) = fs::File::open("input_2.txt") {
+        let lines = io::BufReader::new(file).lines();
+        let re = Regex::new(r"(\d+)\-(\d+) (\w): (\w+)").unwrap();
+        let mut good_pws = 0;
+        for line in lines {
+            if let Ok(line) = line {
+                let cap = re.captures(&line).unwrap();
+                let lb = cap[1].parse::<usize>().unwrap();
+                let ub = cap[2].parse::<usize>().unwrap();
+                let c = &cap[3].chars().nth(0).unwrap();
+                let pw = &cap[4];
+                let count = pw.chars().filter(|x| x == c).count();
+                if count >= lb && count <= ub {
+                    good_pws += 1;
+                }
+            }
+        }
+        println!("({}) good passwords", good_pws);
+    }
+    if let Ok(file) = fs::File::open("input_2.txt") {
+        let lines = io::BufReader::new(file).lines();
+        let re = Regex::new(r"(\d+)\-(\d+) (\w): (\w+)").unwrap();
+        let mut good_pws = 0;
+        for line in lines {
+            if let Ok(line) = line {
+                let cap = re.captures(&line).unwrap();
+                let pos1 = cap[1].parse::<usize>().unwrap();
+                let pos2 = cap[2].parse::<usize>().unwrap();
+                let c = &cap[3].chars().nth(0).unwrap();
+                let pw = &cap[4];
+                if (&pw.chars().nth(pos1-1).unwrap() == c) ^ (&pw.chars().nth(pos2-1).unwrap() == c) {
+                    good_pws += 1;
+                }
+            }
+        }
+        println!("({}) good passwords", good_pws);
+    }
+}
