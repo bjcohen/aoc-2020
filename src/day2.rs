6bb12ae8614dc6748b597331daed43b1515116bf
Ben Cohen <ben@cohen-family.org>
2020-12-02 16:38:34 -0800

    more unwraps

--- src/day2.rs
+++ src/day2.rs
@@ -10,16 +10,15 @@ pub fn day2() -> io::Result<()> {
     let re = Regex::new(r"(\d+)\-(\d+) (\w): (\w+)").unwrap();
     let mut good_pws = 0;
     for line in lines {
-        if let Ok(line) = line {
-            let cap = re.captures(&line).unwrap();
-            let lb = cap[1].parse::<usize>().unwrap();
-            let ub = cap[2].parse::<usize>().unwrap();
-            let c = &cap[3].chars().nth(0).unwrap();
-            let pw = &cap[4];
-            let count = pw.chars().filter(|x| x == c).count();
-            if count >= lb && count <= ub {
-                good_pws += 1;
-            }
+        let line = line.unwrap();
+        let cap = re.captures(&line).unwrap();
+        let lb = cap[1].parse::<usize>().unwrap();
+        let ub = cap[2].parse::<usize>().unwrap();
+        let c = &cap[3].chars().nth(0).unwrap();
+        let pw = &cap[4];
+        let count = pw.chars().filter(|x| x == c).count();
+        if count >= lb && count <= ub {
+            good_pws += 1;
         }
     }
     println!("({}) good passwords", good_pws);
@@ -28,16 +27,14 @@ pub fn day2() -> io::Result<()> {
     let re = Regex::new(r"(\d+)\-(\d+) (\w): (\w+)").unwrap();
     let mut good_pws = 0;
     for line in lines {
-        if let Ok(line) = line {
-            let cap = re.captures(&line).unwrap();
-            let pos1 = cap[1].parse::<usize>().unwrap();
-            let pos2 = cap[2].parse::<usize>().unwrap();
-            let c = &cap[3].chars().nth(0).unwrap();
-            let pw = &cap[4];
-            if (&pw.chars().nth(pos1 - 1).unwrap() == c) ^ (&pw.chars().nth(pos2 - 1).unwrap() == c)
-            {
-                good_pws += 1;
-            }
+        let line = line.unwrap();
+        let cap = re.captures(&line).unwrap();
+        let pos1 = cap[1].parse::<usize>().unwrap();
+        let pos2 = cap[2].parse::<usize>().unwrap();
+        let c = &cap[3].chars().nth(0).unwrap();
+        let pw = &cap[4];
+        if (&pw.chars().nth(pos1 - 1).unwrap() == c) ^ (&pw.chars().nth(pos2 - 1).unwrap() == c) {
+            good_pws += 1;
         }
     }
     println!("({}) good passwords", good_pws);
