0ceb82c0c75ac3332ffd9c2783bd3036a614bb42
Ben Cohen <ben@cohen-family.org>
2020-12-07 22:14:18 -0800

    day8

---
+++ src/day8.rs
@@ -0,0 +1,89 @@
+use anyhow::{bail, ensure, Result};
+use aoc::soln;
+use std::fs;
+
+#[soln]
+pub fn main() -> Result<()> {
+    let contents = fs::read_to_string("input_8.txt").unwrap();
+    let mut lines: Vec<(&str, &str)> = contents.trim().lines().map(|l| {
+        let mut s = l.split(' ');
+        (s.next().unwrap(), s.next().unwrap())
+    }).collect();
+    part1(&lines)?;
+    part2(&mut lines)?;
+    Ok(())
+}
+
+enum RunResult {
+    Loop(i32),
+    Term(i32),
+}
+
+fn run(lines: &Vec<(&str, &str)>) -> Result<RunResult> {
+    let mut acc: i32 = 0;
+    let mut pc: i32 = 0;
+    let mut used: Vec<bool> = vec![false; lines.len()];
+    while pc < lines.len() as i32 {
+        if used[pc as usize] {
+            return Ok(RunResult::Loop(acc));
+        }
+        used[pc as usize] = true;
+        let instr = lines[pc as usize].0;
+        let arg: i32 = lines[pc as usize].1.parse()?;
+        match instr {
+            "acc" => {
+                acc += arg;
+                pc += 1;
+            }
+            "jmp" => pc += arg,
+            "nop" => pc += 1,
+            _ => bail!("Unhandled instruction [{}]", instr),
+        }
+        ensure!(pc > 0, "PC must be greater than 0 but was {}", pc);
+    }
+    Ok(RunResult::Term(acc))
+}
+
+fn part1(lines: &Vec<(&str, &str)>) -> Result<()> {
+    match run(&lines)? {
+        RunResult::Loop(res) => println!("Acc value was ({})", res),
+        _ => bail!("Unexpected Term result"),
+    }
+    Ok(())
+}
+
+fn part2(lines: &mut Vec<(&str, &str)>) -> Result<()> {
+    let mut result = 0;
+    for i in 0..lines.len() {
+        let instr = lines[i].0;
+        let arg = lines[i].1;
+        match instr {
+            "acc" => continue,
+            "jmp" => {
+                lines[i] = ("nop", arg);
+                match run(lines)? {
+                    RunResult::Loop(_) => (),
+                    RunResult::Term(res) => {
+                        result = res;
+                        break;
+                    }
+                }
+                lines[i] = ("jmp", arg);
+            }
+            "nop" => {
+                lines[i] = ("jmp", arg);
+                match run(lines)? {
+                    RunResult::Loop(_) => (),
+                    RunResult::Term(res) => {
+                        result = res;
+                        break;
+                    }
+                }
+                lines[i] = ("nop", arg);
+            }
+            _ => bail!("Unhandled instruction [{}]", instr),
+        }
+    }
+    println!("Acc value was ({})", result);
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,24 @@
 #![feature(iterator_fold_self)]
 
-// mod day1;
-// mod day2;
-// mod day3;
-// mod day4;
-// mod day5;
-// mod day6;
+mod day1;
+mod day2;
+mod day3;
+mod day4;
+mod day5;
+mod day6;
 mod day7;
+mod day8;
 
-use std::io;
+use anyhow;
 
-fn main() -> io::Result<()> {
-    // day1::day1()?;
-    // day2::day2()?;
-    // day3::day3()?;
-    // day4::day4()?;
-    // day5::day5()?;
-    // day6::day6()?;
+fn main() -> anyhow::Result<()> {
+    day1::day1()?;
+    day2::day2()?;
+    day3::day3()?;
+    day4::day4()?;
+    day5::day5()?;
+    day6::day6()?;
     day7::day7()?;
+    day8::main()?;
     Ok(())
 }
